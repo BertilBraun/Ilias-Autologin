b6b6e3892212d9bc19950505897a8fdb44159e2d
Bertil Braun <bertil.braun.private@gmail.com>
2024-04-20 22:16:22 +0200

    Fixed Login for new Design

--- js/loginButton.js
+++ js/loginButton.js
@@ -1,15 +1,23 @@
 var interval = setInterval(
   (function logic() {
-    const button = document.querySelector(
-      '[aria-label="Anmelden"]'
-    ).parentElement;
-
-    console.log("Found Button:", button?.textContent);
-    if (button?.textContent.includes("Anmelden")) {
-      clearInterval(interval);
-      button.click();
+    var links = document.querySelectorAll('a[href*="ilias.studium.kit.edu/login.php"]');
+    var button;
+    
+    for (var i = 0; i < links.length; i++) {
+      if (links[i].textContent.trim() === "Anmelden") {
+        button = links[i];
+        break;
+      }
     }
-
+    
+    // Now you can work with targetLink, for example:
+    if (button) {
+      console.log("Found Button:", button.textContent);
+      if (button.textContent.includes("Anmelden")) {
+        clearInterval(interval);
+        button.click();
+      }
+    }
     return logic;
   })(),
   1000
